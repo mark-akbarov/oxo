b61c62984b77ebec4f09ec5b5195f4b76cfb0807
Umarkhon Akbarov <mark-akbarov@gmail.com>
2022-08-08 12:29:19 +0500

    add category

--- apps/product/models/category.py
+++ apps/product/models/category.py
@@ -9,3 +9,15 @@ class CategoryManager(TreeManager):
     def viewable(self):
         queryset = self.get_queryset().filter(level=0)
         return queryset
+
+
+class Category(MPTTModel, BaseModel):
+    name = models.CharField(max_length=255)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    objects = CategoryManager()
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self) -> str:
+        return self.name
