6667415a561e2c5a4450478f54c0b6e73ca1c7ca
Umarkhon Akbarov <mark-akbarov@gmail.com>
2022-08-08 10:35:36 +0500

    add venv to ignore files

---
+++ apps/user/models/base.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    UZ = "uz"
+    RU = "ru"
+    EN = "en"
+
+    LANGUAGES = ((UZ, UZ), (RU, RU), (EN, EN))
+
+    languages = models.CharField(choices=LANGUAGES, default=UZ, max_length=25)
+    birthday = models.DateField(null=True)
+    about = models.CharField(max_length=255, null=True)
+    is_new = models.BooleanField(default=True)
